c252522c138d67271ea4bf5debeeabe4d047db28
martindepaula12 <martoondepaula@gmail.com>
2020-09-03 20:09:35 -0300

    ejercicios funciones

--- PROYECTOS CODE BLOCKS/ejercicio funciones pruebas/main.c
+++ PROYECTOS CODE BLOCKS/ejercicio funciones pruebas/main.c
@@ -9,12 +9,15 @@ void calcularYMostrarSuma(int numeroUno, int numeroDos);
 int restarNumeros(int numeroUno, int NumeroDos);
 void mostrarDatoResta (int datoParametro);
 void calcularYMostrarResta(int numeroUno, int numeroDos);
-int dividirNumeros(int numeroUno, int numeroDos);
-void mostrarDivision(int datoDivision);
+float dividirNumeros(int numeroUno, int numeroDos);
+void mostrarDivision(float datoDivision);
 void calcularYMostrarDivision(int numeroUno, int numeroDos);
 int multiplicarNumeros(int numeroUno, int numeroDos);
-void mostrarMultiplicacion(datoMultiplicacion);
+void mostrarMultiplicacion(int datoMultiplicacion);
 void calcularYMostrarMultiplicacion(int numeroUno, int numeroDos);
+
+
+
 
 int main()
 {
@@ -33,62 +36,89 @@ int main()
     calcularYMostrarSuma(numeroUno, numeroDos);
     calcularYMostrarResta(numeroUno, numeroDos);
     calcularYMostrarDivision(numeroUno, numeroDos);
-    //calcularYmostrarMultiplicacion(numeroUno, numeroDos);
+    calcularYMostrarMultiplicacion(numeroUno, numeroDos);
 
 
+
+
+return 0;
 
 
 }
 
-int sumarNumeros(numeroUno, numeroDos){
+
+
+
+
+int sumarNumeros(int numeroUno, int numeroDos){
 int resultadoSuma;
 resultadoSuma = numeroUno + numeroDos;
 return resultadoSuma;
 
 }
 
-void mostrarDatoEnteroSuma( datoParametro){
+void mostrarDatoEnteroSuma( int datoParametro){
 printf("la suma es: %d ", datoParametro);
 }
 
-void calcularYMostrarSuma( numeroUno,numeroDos){
+void calcularYMostrarSuma( int numeroUno,int numeroDos){
 int resultadoSuma;
 resultadoSuma = sumarNumeros(numeroUno, numeroDos);
 mostrarDatoEnteroSuma(resultadoSuma);
 
 }
 
-int restarNumeros(numeroUno, numeroDos){
+int restarNumeros(int numeroUno,int numeroDos){
 int resultadoResta;
 resultadoResta = numeroUno - numeroDos;
 return resultadoResta;
 }
 
-void mostrarDatoResta(datoParametro){
+void mostrarDatoResta(int datoParametro){
 printf("la resta es: %d", datoParametro);
 }
 
-void calcularYMostrarResta(numeroUno, numeroDos){
+void calcularYMostrarResta(int numeroUno,int  numeroDos){
 int resultado;
 resultado = restarNumeros(numeroUno, numeroDos);
 mostrarDatoResta(resultado);
 }
 
-int dividirNumeros(numeroUno, numeroDos){
+float dividirNumeros(int numeroUno,int numeroDos){
 float resultadoDivision;
 resultadoDivision = (float) numeroUno / numeroDos;
 return resultadoDivision;
 
 }
-void mostrarDivision(datoDivision){
-printf("\n la division es: %d", datoDivision);
+void mostrarDivision(float datoDivision){
+printf("\n la division es: %f", datoDivision);
 }
 
-void calcularYMostrarDivision(numeroUno, numeroDos){
+void calcularYMostrarDivision(int numeroUno, int numeroDos){
 float resultadoDivision;
 resultadoDivision = dividirNumeros(numeroUno, numeroDos);
 mostrarDivision(resultadoDivision);
 }
+
+int multiplicarNumeros(int numeroUno,int numeroDos){
+int resultado;
+resultado = numeroUno * numeroDos;
+return resultado;
+}
+
+void mostrarMultiplicacion(int datoMultiplicacion){
+printf("\n la multiplicacion es: %d", datoMultiplicacion);
+}
+
+void calcularYMostrarMultiplicacion(int numeroUno,int  numeroDos){
+int resultado;
+resultado = multiplicarNumeros(numeroUno, numeroDos);
+mostrarMultiplicacion(resultado);
+}
+
+
+
+
 
 
 
